ac313041c15b05cbc3217f24728a2b9000b99a6e
GEM <guillaume@maiano.fr>
2019-12-13 17:15:39 +0100

    This manager will be exposed to the View Model. That's the part that integrates all the logic (models and mdoel utilities) with the UI-facing elements.

---
+++ WeatherDisplayerLBC/Models/WeatherManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherManager.swift
+//  WeatherDisplayerLBC
+//
+//  Created by guillaume MAIANO on 13/12/2019.
+//  Copyright © 2019 guillaume MAIANO. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    
+}
---
+++ WeatherDisplayerLBCTests/WeatherManagerTests.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherManagerTests.swift
+//  WeatherDisplayerLBCTests
+//
+//  Created by guillaume MAIANO on 13/12/2019.
+//  Copyright © 2019 guillaume MAIANO. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherDisplayerLBC
+
+class WeatherManagerTests: XCTestCase {
+    
+    func testWeatherGrabber_WeatherManager_Creation() {
+        let weatherManager = WeatherManager()
+    }
+
+}
